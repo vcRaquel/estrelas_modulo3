64ba9459de5a6ef3ae1896a9e81f9cb97054e326
Raquel Leão <vc.raquel@gmail.com>
2021-09-17 15:10:43 -0300

    criando um objeto com a classe bola e aprimorando sout

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -7,5 +7,9 @@ public class Main {
 
         System.out.println(obj_contato.nome);
         System.out.println(obj_contato.email);
+
+        Bola quadrada = new Bola(2, "colorida", "Kiko", 3.5);
+        System.out.println("Bola marca: " + quadrada.marca);
+        System.out.println("Cor da bola: " + quadrada.cor);
     }
 }
