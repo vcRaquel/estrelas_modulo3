79100ec998d1bf445ad384fee06cd510ddfafbd3
Raquel Leão <vc.raquel@gmail.com>
2021-09-17 14:58:56 -0300

    criando objeto com a classe e sout de informações

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -3,6 +3,9 @@ package br.com.zup;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Contato obj_contato = new Contato("Raquel", "1234567", "raquel@raquel", 36);
+
+        System.out.println(obj_contato.nome);
+        System.out.println(obj_contato.email);
     }
 }
