43d6504aa9e54566198160521e045638caacd578
Raquel Leão <vc.raquel@gmail.com>
2021-09-17 14:48:53 -0300

    classe Contato e metódo contrutor

---
+++ src/br/com/zup/Contato.java
@@ -0,0 +1,15 @@
+package br.com.zup;
+
+public class Contato {
+    String nome;
+    String telefone;
+    String email;
+    int idade;
+
+    public Contato(String nome, String telefone, String email, int idade){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.idade = idade;
+    }
+}
