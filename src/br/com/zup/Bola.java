081e7c5d458594f53aa93a050b5e83fe56dfac90
Raquel Leão <vc.raquel@gmail.com>
2021-09-17 15:05:10 -0300

    criando classe Bola e metódo construtor

---
+++ src/br/com/zup/Bola.java
@@ -0,0 +1,15 @@
+package br.com.zup;
+
+public class Bola {
+    double circunferencia;
+    String cor;
+    String marca;
+    double velocidade;
+
+    public Bola(double circunferencia,String cor, String marca, double velocidade) {
+        this.circunferencia = circunferencia;
+        this.cor = cor;
+        this.marca = marca;
+        this.velocidade = velocidade;
+    }
+}
